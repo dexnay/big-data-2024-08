34ffa3a3a2caee50fe57de94b733eb1d6061e600
a.polyakov <i@a-polyakov.ru>
2024-09-10 11:26:00 +0300

    map-reduce example 1

--- map-reduce/task1/task1mapper.py
+++ map-reduce/task1/task1mapper.py
@@ -3,4 +3,9 @@
 import sys
 
 for line in sys.stdin:
-    print("")
+    input_data = line.strip().split('  ')
+    year = input_data[0]
+    data = input_data[1:-1]
+    for month in range(len(data)):
+        value = data[month]
+        print(f"{year}\t{value},{month}")
--- map-reduce/task1/task1reducer.py
+++ map-reduce/task1/task1reducer.py
@@ -2,6 +2,26 @@
 
 import sys
 
+max_value = 0
+min_value = 0
+max_year = None
+min_year = None
+max_month = -1
+min_month = -1
+
 for line in sys.stdin:
-    print(f"{max_year} {max_month} {max_value}\n")
-    print(f"{min_year} {min_month} {min_value}\n")
+    year, value = line.strip().split('\t')
+    count, month = value.strip().split(',')
+    count = int(count)
+
+    if max_value <= count:
+        max_value = count
+        max_year = year
+        max_month = int(month)
+    if min_value >= count:
+        min_value = count
+        min_year = year
+        min_month = int(month)
+
+print(f"{max_year} {max_month} {max_value}")
+print(f"{min_year} {min_month} {min_value}")
