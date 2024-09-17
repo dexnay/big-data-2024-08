2bc3a7ef3f501781648f0396d1d9c5ae9c8ceb01
a.polyakov <i@a-polyakov.ru>
2024-09-17 10:05:18 +0300

    add file formats example

--- file-formats/avroExample.py
+++ file-formats/avroExample.py
@@ -1,14 +1,20 @@
-import avro
+import avro.schema
 
 from avro.datafile import DataFileWriter
 from avro.io import DatumWriter
+
+file_schema = avro.schema.parse(open("employee.avsc", 'r').read())
+print(file_schema)
 
 employee = {
     "employee_id": 12,
     "employee_name": 'Andrey',
     "joining_date": 20240101,
-    "salary": 123,
+    "salary": 12300.5,
     "address": "test",
     "update_date_time": 20240101010101
 }
 
+writer = DataFileWriter(open("employee.avro", "wb"), DatumWriter(), file_schema)
+writer.append(employee)
+writer.close()
