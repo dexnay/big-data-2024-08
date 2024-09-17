941ef735b92150a8275f9aeb8f3eb29cf23a71e4
a.polyakov <i@a-polyakov.ru>
2024-09-17 10:45:55 +0300

    add file formats example

--- file-formats/parquetExample.py
+++ file-formats/parquetExample.py
@@ -1,7 +1,8 @@
 import pyarrow as pa
 from pyarrow import parquet
+import pandas as pd
 
-pa.table({
+employee_table = pa.table({
     "employee_id": [12],
     "employee_name": ['Andrey'],
     "joining_date": [20240101],
@@ -9,4 +10,8 @@ pa.table({
     "address": ["test"],
     "update_date_time": [20240101010101]
 })
-parquet.write_table("employee.parquet")
+parquet.write_table(employee_table, "employee.parquet")
+
+table = parquet.read_table("employee.parquet", columns=['employee_id', 'employee_name', 'salary'])
+employee_df = table.to_pandas()
+print(employee_df.head())
